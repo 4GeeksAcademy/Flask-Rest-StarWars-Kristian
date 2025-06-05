9ab2c9c4e535c3af8ebac434072f9352984603a4
Kristian Pacheco <kristianpachecar18@gmail.com>
2025-06-05 03:57:40 +0000

    Added more info to the User class

--- src/models.py
+++ src/models.py
@@ -9,18 +9,33 @@ class User(db.Model):
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    name: Mapped[str] = mapped_column(String(120), nullable=False)
+    address: Mapped[str] = mapped_column(String(120), nullable=False)
+    country: Mapped[str] = mapped_column(String(120), nullable=False)
 
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "name": self.name,
+            "address": self.address,
+            "country": self.country
             # do not serialize the password, its a security breach
         }
 
 class Character(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    height: Mapped[str] = mapped_column(nullable=False)
-    gender: Mapped[str] = mapped_column(String(), nullable=False)
-    eye_color: Mapped[str] = mapped_column(String(), nullable=False)
+    height: Mapped[str] = mapped_column(String(120), nullable=False)
+    gender: Mapped[str] = mapped_column(String(120), nullable=False)
+    eye_color: Mapped[str] = mapped_column(String(120), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "height": self.height,
+            "gender": self.gender,
+            "eye_color": self.eye_color
+        }
