a4b3dec06590dc2c6fc3b3f978cdce27045de6ae
Kristian Pacheco <kristianpachecar18@gmail.com>
2025-06-09 22:49:31 +0000

    Added GET endpoints

--- src/app.py
+++ src/app.py
@@ -8,15 +8,16 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User, Character
-#from models import Person
+from models import db, User, Character, Planet, Vehicle, Fav_character, Fav_planet, Fav_vehicle
+# from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace(
+        "postgres://", "postgresql://")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -27,14 +28,19 @@ CORS(app)
 setup_admin(app)
 
 # Handle/serialize errors like a JSON object
+
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
+
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
+
 
 @app.route('/users', methods=['GET'])
 def get_users():
@@ -43,22 +49,48 @@ def get_users():
 
         if not query_results:
             return jsonify({'msg': 'No users found'}), 400
-        
-        results = list(map(lambda item: item.serialize(), query_results))
+
+        users = list(map(lambda item: item.serialize(), query_results))
 
         response_body = {
             'msg': 'ok',
-            'results': results
+            'results': users
         }
 
         return jsonify(response_body), 200
-    
+
     except Exception as e:
         return jsonify({
             'msg': f'Internal server error',
             'error': {str(e)}
         }), 500
-    
+
+@app.route('/users/<int:id>/favorites', methods=['GET'])
+def get_favorites(id):
+    try:
+        query_result = User.query.get(id)
+
+        if not query_result:
+            return jsonify({'msg': 'No user found'}), 404
+        
+        characters = query_result.favorite_character
+
+
+        favorites = list(map(lambda item: item.serialize(), query_result))
+
+        response_body = {
+            'msg': 'ok',
+            'results': favorites
+        }
+
+        return jsonify(response_body), 200
+
+    except Exception as e:
+        return jsonify({
+            'msg': f'Internal server error',
+            'error': {str(e)}
+        }), 500
+
 @app.route('/characters', methods=['GET'])
 def get_characters():
     try:
@@ -66,34 +98,78 @@ def get_characters():
 
         if not query_results:
             return jsonify({'msg': 'No characters found'}), 400
-        
-        results = list(map(lambda item: item.serialize(), query_results))
+
+        characters = list(map(lambda item: item.serialize(), query_results))
         response_body = {
             'msg': 'ok',
-            'results': results
+            'results': characters
         }
         return jsonify(response_body), 200
-    
+
     except Exception as e:
         return ({
             'msg': f'Internal server error',
             'error': {str(e)}
         }), 500
-    
-@app.route('/characters/<int:id>', methods=['GET'])
-def get_character_id():
-    try:
-        query_results = Character.query.get(id)
 
-        if not query_results:
+
+@app.route('/characters/<int:id>', methods=['GET'])
+def get_character_id(id):
+    try:
+        character = Character.query.get(id)
+
+        if not character:
             return jsonify({
                 'msg': 'Character not found'
             }), 400
-        
-        results = list(map(lambda item: item.serialize(), query_results))
+
         response_body = {
             'msg': 'ok',
-            'results': results
+            'result': character.serialize()
+        }
+        return jsonify(response_body)
+    except Exception as e:
+        return jsonify({
+            'msg': f'Internal server error',
+            'error': {str(e)}
+        }), 500
+
+
+@app.route('/planets', methods=['GET'])
+def get_planets():
+    try:
+        query_results = Planet.query.all()
+
+        if not query_results:
+            return jsonify({'msg': 'No planets found'}), 400
+
+        planets = list(map(lambda item: item.serialize(), query_results))
+        response_body = {
+            'msg': 'ok',
+            'results': planets
+        }
+        return jsonify(response_body), 200
+
+    except Exception as e:
+        return ({
+            'msg': f'Internal server error',
+            'error': {str(e)}
+        }), 500
+
+
+@app.route('/planets/<int:id>', methods=['GET'])
+def get_planet_id(id):
+    try:
+        planet = Planet.query.get(id)
+
+        if not planet:
+            return jsonify({
+                'msg': 'Planet not found'
+            }), 400
+
+        response_body = {
+            'msg': 'ok',
+            'result': planet.serialize()
         }
         return jsonify(response_body)
     except Exception as e:
@@ -115,11 +191,11 @@ def create_user():
     created_user = User.query.filter_by(email=email).first()
     if created_user:
         return jsonify({'msg': 'Email is assigned to a created user already'}), 409
-    
+
     new_user = User(
-        email = email,
-        password = password,
-        is_active = is_active
+        email=email,
+        password=password,
+        is_active=is_active
     )
     db.session.add(new_user)
 
@@ -129,7 +205,7 @@ def create_user():
     except Exception as e:
         db.session.rollback()
         return jsonify({'msg': f'Internal Server Error', 'error': {str(e)}}), 500
-        
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
